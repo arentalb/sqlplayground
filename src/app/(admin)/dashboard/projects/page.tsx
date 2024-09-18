a7ab86c7b71c6b80f17611d490abdbd39951747f
aren talb <aren.talb00@gmail.com>
2024-09-18 16:43:16 +0300

    Design the simple layout for projects page

--- src/app/(admin)/dashboard/projects/page.tsx
+++ src/app/(admin)/dashboard/projects/page.tsx
@@ -1,3 +1,33 @@
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>projects page</div>;
+  return (
+    <div className={"px-10 py-6"}>
+      <div className={" py-2"}>
+        <Button>Create New Project </Button>
+      </div>
+      <div
+        className={
+          " pt-2 grid  grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4"
+        }
+      >
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </div>
+  );
+}
+
+function ProjectCard() {
+  return (
+    <div
+      className={
+        "border rounded-3xl px-8 py-8 hover:border-violet-600 transition-all duration-300"
+      }
+    >
+      card{" "}
+    </div>
+  );
 }
