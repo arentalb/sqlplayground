cf8b751b6254c6d47ce4c7fcffc280e8abd76339
aren talb <aren.talb00@gmail.com>
2024-09-18 18:44:35 +0300

    Implement server action for creating new database and new project

--- src/actions/project.action.ts
+++ src/actions/project.action.ts
@@ -2,6 +2,8 @@
 
 import { CreateProjectData, CreateProjectSchema } from "@/lib/schemas";
 import { createErrorResponse, createSuccessResponse } from "@/lib/response";
+import db from "@/lib/db";
+import { getAuth } from "@/lib/auth/getAuth";
 
 export async function createProject(project: CreateProjectData) {
   const validatedFields = CreateProjectSchema.safeParse(project);
@@ -10,6 +12,35 @@ export async function createProject(project: CreateProjectData) {
     return createErrorResponse("Validation error");
   }
 
-  console.log(validatedFields);
-  return createSuccessResponse("Project created ");
+  try {
+    const { user } = await getAuth();
+    if (!user || !user.id) {
+      return createErrorResponse("User does not exist");
+    }
+
+    const title = validatedFields.data.title;
+    const description = validatedFields.data.description;
+    const databaseName = validatedFields.data.database_name;
+
+    await db.$executeRawUnsafe(`CREATE DATABASE "${databaseName}"`);
+
+    const newProject = await db.project.create({
+      data: {
+        database_name: databaseName,
+        title,
+        description,
+        database_url: `postgresql://postgres:12345@localhost:5432/${databaseName}`,
+        owner_id: user.id,
+      },
+    });
+
+    console.log("Project created:", newProject);
+    return createSuccessResponse("Project created successfully");
+  } catch (error: any) {
+    if (error.code === "P2010") {
+      return createErrorResponse("Database with that name already exists");
+    }
+    console.error("Error creating project:", error);
+    return createErrorResponse("Failed to create project");
+  }
 }
