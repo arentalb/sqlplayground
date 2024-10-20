44a0eb921f5079fc5d925d6c55232ef703ebab6d
aren talb <aren.talb00@gmail.com>
2024-10-20 22:06:34 +0300

    Add other menu items

--- src/app/(admin)/_components/sideNav.tsx
+++ src/app/(admin)/_components/sideNav.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import Logo, { LOGO_SIZE } from "@/components/logo";
-import { CircleUserRound, FolderKanban, House, Settings } from "lucide-react";
+import {
+  BookOpen,
+  Cable,
+  Flag,
+  FolderKanban,
+  Home,
+  Settings,
+  UserCircle,
+} from "lucide-react";
 import NavLink from "@/app/(admin)/_components/navLink";
 import { useState } from "react";
 import SideNavToggle from "@/app/(admin)/_components/sideNavToggler";
@@ -11,21 +19,23 @@ export default function SideNav() {
 
   return (
     <div
-      className={`hidden sm:flex relative  z-10 p-4 h-full  flex-col  no-scrollbar ${isOpen ? "w-1/6 items-start" : "w-[100px] items-center"}`}
+      className={`hidden sm:flex relative z-10 p-4 h-full flex-col no-scrollbar ${
+        isOpen ? "w-1/6 items-start" : "w-[100px] items-center"
+      }`}
     >
       <SideNavToggle
         isOpen={isOpen}
         setIsOpen={setIsOpen}
-        className="absolute  z-20 right-0 bottom-5 translate-x-5"
+        className="absolute z-20 right-0 bottom-5 translate-x-5"
       />
-      <div className="flex  items-center justify-between">
+      <div className="flex items-center justify-between">
         <Logo size={!isOpen ? LOGO_SIZE.SMALL : LOGO_SIZE.BIG} />
       </div>
       <ul className="flex w-full flex-col h-full mt-8 gap-4">
         <li>
           <NavLink
             href={"/dashboard"}
-            icon={<House />}
+            icon={<Home />}
             text={"Home"}
             textShown={isOpen}
           />
@@ -40,6 +50,30 @@ export default function SideNav() {
         </li>
         <li>
           <NavLink
+            href={"/dashboard/connections"}
+            icon={<Cable />}
+            text={"Connections"}
+            textShown={isOpen}
+          />
+        </li>
+        <li>
+          <NavLink
+            href={"/dashboard/handbook"}
+            icon={<BookOpen />}
+            text={"Handbook"}
+            textShown={isOpen}
+          />
+        </li>
+        <li>
+          <NavLink
+            href={"/dashboard/challenges"}
+            icon={<Flag />}
+            text={"Challenges"}
+            textShown={isOpen}
+          />
+        </li>
+        <li>
+          <NavLink
             href={"/dashboard/settings"}
             icon={<Settings />}
             text={"Settings"}
@@ -49,7 +83,7 @@ export default function SideNav() {
         <li className="mt-auto">
           <NavLink
             href={"/dashboard/profile"}
-            icon={<CircleUserRound />}
+            icon={<UserCircle />}
             text={"Profile"}
             classname={"mt-auto"}
             textShown={isOpen}
